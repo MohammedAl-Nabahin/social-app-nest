6cc1664629b7774a548e07d3d72f2649c277be7a
Mohammedal-nabahin <moha.nabahin@gmail.com>
2023-08-30 13:05:22 +0300

    Edit database provider

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -4,6 +4,10 @@ import { Post } from '../post/model/post.model';
 import { Comment } from '../comment/model/comment.model';
 import { User } from '../user/model/user.model';
 import { DATABASE, SEQUELIZE } from 'src/common/constants/constant';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger();
+
 export const databaseProviders = [
   {
     provide: SEQUELIZE,
@@ -11,6 +15,16 @@ export const databaseProviders = [
       const config = configService.get(DATABASE);
       const sequelize = new Sequelize(config);
       sequelize.addModels([User, Post, Comment]);
+
+      sequelize
+        .authenticate()
+        .then(() => {
+          logger.log('Database connection established');
+        })
+        .catch((error) => {
+          logger.error(`Database Connection Error: ${error.message}`);
+        });
+
       return sequelize;
     },
 
